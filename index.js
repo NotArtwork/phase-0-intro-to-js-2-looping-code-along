2b3fe73f9ecd0ed4a4e0d6863acf8306a3f3776d
Antonio <antarce3@gmail.com>
2022-06-17 18:36:13 -0400

    commit tree 04d351e618d2b7f48e7c6e97844fd7ddb0171630

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+const newArray = ["Antonio", "Klara", "Justin"];
+
+
+
+function writeCards(newArray, eventName) {
+    const array = []
+    for (let i = 0; i < newArray.length; i++) {
+//        console.log(`Thank you, ${newArray[i]}, for the wonderful birthday gifts!`);
+        array.push(`Thank you, ${newArray[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return array
+}
+
+
+
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+        console.log(i)
+        i--;
+    }
+ 
+}
+//function writeCards(newArray) {
+//    let i = 0;
+//    while (i < newArray.length); {
+//       console.log(`Thank you, ${newArray[i]}, for the wonderful birthday gift!`);
+//        i++;
+//   }
+//
+//return newArray;
+// }
